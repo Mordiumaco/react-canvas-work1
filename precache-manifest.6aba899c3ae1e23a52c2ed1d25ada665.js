2be788c1928f9401558c8668b11951d2f021e313
mordiumaco <mordiumaco@gmail.com>
2020-08-27 01:54:36 +0900

    Updates

---
+++ precache-manifest.6aba899c3ae1e23a52c2ed1d25ada665.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0b251cfca816bb2e2ecdbffb9b61d1ba",
+    "url": "/testing/index.html"
+  },
+  {
+    "revision": "63e6e3a1c48ebe2ba414",
+    "url": "/testing/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e0f3d521a5d6a1966991",
+    "url": "/testing/static/js/2.af86a2b5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/testing/static/js/2.af86a2b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63e6e3a1c48ebe2ba414",
+    "url": "/testing/static/js/main.19ef6717.chunk.js"
+  },
+  {
+    "revision": "224fa7e8035c889a86cc",
+    "url": "/testing/static/js/runtime-main.7bbf28cf.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/testing/static/media/logo.5d5d9eef.svg"
+  }
+]);
